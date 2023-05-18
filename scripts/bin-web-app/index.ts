1e2b5f4e008cb4d8d7475fc2893eb26cdcaa6248
Connor Burton <me@connorburton.com>
2023-05-18 22:18:15 +0100

    feat: add bin web app to perform request testing against

---
+++ scripts/bin-web-app/index.ts
@@ -0,0 +1,24 @@
+const server = Deno.listen({ port: 8080 });
+console.log('HTTP bin webserver running. Access it as: http://localhost:8080/');
+
+for await (const conn of server) {
+	serveHttp(conn);
+}
+
+async function serveHttp(conn: Deno.Conn) {
+	const httpConn = Deno.serveHttp(conn);
+
+	for await (const requestEvent of httpConn) {
+		const body = `Your user-agent is:\n\n${
+			requestEvent.request.headers.get("user-agent") ?? "Unknown"
+		}`;
+		
+		console.log(`Received request to ${requestEvent.request.method} ${requestEvent.request.url}`);
+
+    	requestEvent.respondWith(
+			new Response(body, {
+				status: 200,
+			}),
+      	);
+    }
+}
