5538cb042071ca43f077c34aafba412b76c05b76
Connor Burton <me@connorburton.com>
2023-05-21 15:09:28 +0100

    feat: minimum working example of load testing based on real data

--- index.ts
+++ index.ts
@@ -1,39 +1,31 @@
-// import { DB } from "https://deno.land/x/sqlite/mod.ts";
-
-// // Open a database
-// const db = new DB("database.db");
-// db.execute(`
-//   CREATE TABLE IF NOT EXISTS people (
-//     id INTEGER PRIMARY KEY AUTOINCREMENT,
-//     name TEXT
-//   )
-// `);
-
-// // Run a simple query
-// for (const name of ["Peter Parker", "Clark Kent", "Bruce Wayne"]) {
-//   db.query("INSERT INTO people (name) VALUES (?)", [name]);
-// }
-
-// // Print out data in table
-// for (const [name] of db.query("SELECT name FROM people")) {
-//   console.log(name);
-// }
-
-// // Close connection
-// db.close();
-
 import testData from './test-data.json' assert { type: "json" };
 
-const total = testData.length;
-let success = 0;
-let failure = 0;
+const timestampToNumber = (data) => {
+  return {
+    ...data,
+    timestamp: {
+      us: Number(data.timestamp.us)
+    }
+  }
+}
 
-performance.mark('start-requests');
+const calculateDeltas = (data) => data.map(({ timestamp }) => timestamp.us - data[0].timestamp.us);
 
-for await (const data of testData) {
+const wrapFinish = async (fn) => {
+  await fn();
+  performance.mark('finish-requests');
+
+  console.log(`Finished making requests after ${performance.measure('requests', 'start-requests', 'finish-requests').duration}ms, ${success}/${total} succeeded, ${failure} failed`);
+}
+
+const sendRequest = async (data, scheduled) => {
+  const now = new Date().getTime();
+  console.log(`Called sendRequest, was scheduled at ${scheduled}, is now ${now}, took ${now - scheduled}ms between scheduling and triggering`);
   const url = new URL(`http://bin-web-app:8080/${data.url.path}`);
+  console.log(`Sending request to ${data.http.request.method} ${url} at ${new Date()}`);
   const resp = await fetch(url, {
-    method: data.http.request.method
+    method: data.http.request.method,
+    headers: data.http.request.headers
   });
 
   console.log(`Request to ${data.http.request.method} ${url} was successful with status code ${resp.status}`);
@@ -43,8 +35,37 @@ for await (const data of testData) {
   } else {
     failure++;
   }
-}
+};
 
-performance.mark('finish-requests');
+// STEP 1:
+// - Take data from any source
+// - Convert it into a structure we expect
+// - Structure should normalize the timestamp to be a delta
 
-console.log(`Finished making requests after ${performance.measure('requests', 'start-requests', 'finish-requests').duration}ms, ${success}/${total} succeeded, ${failure} failed`);
+const sortedData = testData.map(timestampToNumber).sort((a, b) => a.timestamp.us - b.timestamp.us);
+const deltas = calculateDeltas(sortedData);
+console.log('Length of deltas is ', deltas.length);
+console.log('Length of data is ', sortedData.length);
+
+// STEP 2:
+// - Set timeouts for every timestamp
+// - Each timeout will perform all the requests for that millisecond
+// - As we start at 0 the timers will start executing as we start setting them
+// - TODO: Batch these so we don't set thousands of timers
+const total = sortedData.length;
+let success = 0;
+let failure = 0;
+
+performance.mark('start-requests');
+
+console.log('Starting requests');
+
+const timers: number[] = [];
+
+deltas.forEach((delta, idx) => {
+  const send = sendRequest.bind(null, sortedData[idx], new Date().getTime());
+  const callback = idx === deltas.length - 1 ? wrapFinish.bind(null, send) : send;
+  timers.push(setTimeout(callback, delta));
+});
+
+console.log(`Set all timers, currently have ${timers.length}`);
