3f582199acb54fa38f6cdc044cbd2497620c0404
Connor Burton <me@connorburton.com>
2023-08-13 17:59:43 +0100

    feat: measure jitter in deno script

--- index.ts
+++ index.ts
@@ -3,32 +3,38 @@ import testData from './test-data.json' assert { type: "json" };
 const timestampToNumber = (data) => {
   return {
     ...data,
-    timestamp: {
-      us: Number(data.timestamp.us)
-    }
+    '@timestamp': new Date(data['@timestamp']).getTime()
   }
 }
 
-const calculateDeltas = (data) => data.map(({ timestamp }) => timestamp.us - data[0].timestamp.us);
+const calculateDeltas = (data) => data.map(({ '@timestamp': timestamp }) => timestamp - data[0]['@timestamp']);
 
 const wrapFinish = async (fn) => {
   await fn();
   performance.mark('finish-requests');
 
   console.log(`Finished making requests after ${performance.measure('requests', 'start-requests', 'finish-requests').duration}ms, ${success}/${total} succeeded, ${failure} failed`);
+  const sortedJitter = jitter.sort((a, b) => a - b);
+  console.log('Jitter stats:');
+  console.log(`Average ${sortedJitter.reduce((a, b) => a + b, 0) / sortedJitter.length}ms`);
+  console.log(`Minimum ${sortedJitter[0]}ms`);
+  console.log(`Maximum ${sortedJitter[jitter.length - 1]}ms`);
 }
 
-const sendRequest = async (data, scheduled) => {
+const sendRequest = async (data, scheduled, delta) => {
   const now = new Date().getTime();
-  console.log(`Called sendRequest, was scheduled at ${scheduled}, is now ${now}, took ${now - scheduled}ms between scheduling and triggering`);
+  const delay = (now - scheduled) - delta;
+  // console.log(`Called sendRequest, was scheduled at ${scheduled}, is now ${now}, took ${now - scheduled}ms (jitter ${delay}ms, original delta ${delta}ms) between scheduling and triggering`);
+  jitter.push(delay);
+
   const url = new URL(`http://bin-web-app:8080/${data.url.path}`);
-  console.log(`Sending request to ${data.http.request.method} ${url} at ${new Date()}`);
+  // console.log(`Sending request to ${data.http.request.method} ${url} at ${new Date()}`);
   const resp = await fetch(url, {
     method: data.http.request.method,
     headers: data.http.request.headers
   });
 
-  console.log(`Request to ${data.http.request.method} ${url} was successful with status code ${resp.status}`);
+  // console.log(`Request to ${data.http.request.method} ${url} was successful with status code ${resp.status}`);
 
   if (resp.status === 200) {
     success++;
@@ -42,10 +48,13 @@ const sendRequest = async (data, scheduled) => {
 // - Convert it into a structure we expect
 // - Structure should normalize the timestamp to be a delta
 
-const sortedData = testData.map(timestampToNumber).sort((a, b) => a.timestamp.us - b.timestamp.us);
+const sortedData = testData.map(timestampToNumber).sort((a, b) => a['@timestamp'] - b['@timestamp']);
 const deltas = calculateDeltas(sortedData);
+console.log('start', new Date(sortedData[0]['@timestamp']), 'end', new Date(sortedData[sortedData.length - 1]['@timestamp']));
 console.log('Length of deltas is ', deltas.length);
 console.log('Length of data is ', sortedData.length);
+
+console.log(`Should take ${deltas[deltas.length - 1]}ms to finish`);
 
 // STEP 2:
 // - Set timeouts for every timestamp
@@ -55,6 +64,7 @@ console.log('Length of data is ', sortedData.length);
 const total = sortedData.length;
 let success = 0;
 let failure = 0;
+const jitter: number[] = [];
 
 performance.mark('start-requests');
 
@@ -63,7 +73,7 @@ console.log('Starting requests');
 const timers: number[] = [];
 
 deltas.forEach((delta, idx) => {
-  const send = sendRequest.bind(null, sortedData[idx], new Date().getTime());
+  const send = sendRequest.bind(null, sortedData[idx], new Date().getTime(), delta);
   const callback = idx === deltas.length - 1 ? wrapFinish.bind(null, send) : send;
   timers.push(setTimeout(callback, delta));
 });
